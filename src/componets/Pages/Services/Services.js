e9968937962bf8811ba19959789b8e5b908d5599
PRONOY <pronoybanik82@gmail.com>
2022-12-14 12:19:01 +0600

    name change

--- src/componets/Pages/Services/Services.js
+++ src/componets/Pages/Services/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h2 className='text-4xl font-bold text-center my-16 font-serif'>Our Products</h2>
+            <h2 className='text-4xl font-bold text-center my-16 font-serif'>Our Products is sceond hand</h2>
            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ml-4'>
            {
                 servicesData.map(services => <ServiceItem
